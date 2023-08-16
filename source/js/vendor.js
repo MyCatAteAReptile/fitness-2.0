a694b657133d1694bc9d51d282714a5c86c678b7
MyCatAteAReptile <114024484+MyCatAteAReptile@users.noreply.github.com>
2023-08-16 20:19:39 +0500

    fixes after testing

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -17,18 +17,18 @@ export const coachSlider = new Swiper('.coaches__slider', {
   spaceBetween: 40,
   // Responsive breakpoints
   breakpoints: {
-    // when window width is >= 320px
+    // when window width is >= 0px
     0: {
       slidesPerView: 1,
       spaceBetween: 0,
     },
-    // when window width is >= 480px
+    // when window width is >= 768px
     768: {
       slidesPerView: 2,
       spaceBetween: 30,
     },
-    // when window width is >= 640px
-    1220: {
+    // when window width is >= 1200px
+    1200: {
       slidesPerView: 4,
       spaceBetween: 40,
     },
